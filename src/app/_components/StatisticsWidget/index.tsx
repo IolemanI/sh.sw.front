8de35251f38ff084bf9a4e68ecb54f42ea794621
Oleksii Carlini <oleksii.carlini@gmail.com>
2024-08-04 02:00:44 +0300

    implemented statistics widget

---
+++ src/app/_components/StatisticsWidget/index.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { Box, Container, Divider, Grid, Stack, Typography } from '@mui/material';
+
+export default function StatisticsWidget() {
+  return (
+    <section style={{ marginTop: 50, paddingBottom: 60 }}>
+      <Container maxWidth="md">
+        <Grid
+          container
+          sx={(theme) => ({
+            display: 'flex',
+            height: 200,
+            color: 'white',
+            backgroundColor: theme.palette.primary.main,
+            background: 'linear-gradient(90deg, hsla(213, 100%, 30%, 1) 20%, hsla(213, 100%, 50%, 1) 100%)',
+            borderRadius: theme.shape.borderRadius,
+            padding: 2,
+          })}
+        >
+          <Grid item xs={4} display="flex" justifyContent="center" alignItems="center" p={2}>
+            <Typography variant="h5" fontWeight={500} component="p">
+              Delivering cutting-edge technology solutions for over two years
+            </Typography>
+          </Grid>
+
+          <Grid item xs={8}>
+            <Stack height="100%" direction="row" justifyContent="space-between">
+              <Box width="100%" display="flex" flexDirection="column" p={2}>
+                <Typography variant="h3" component="p" fontWeight={500}>100+</Typography>
+                <Typography variant="subtitle1" component="p" fontWeight={400}>success stories in our portfolio</Typography>
+              </Box>
+              <Divider flexItem orientation="vertical" sx={{ width: 2, background: 'white' }}/>
+              <Box width="100%" display="flex" flexDirection="column" p={2}>
+                <Typography variant="h3" component="p" fontWeight={500}>50+</Typography>
+                <Typography variant="subtitle1" component="p" fontWeight={400}>successfully delivered projects for startups</Typography>
+              </Box>
+              <Divider flexItem orientation="vertical" sx={{ width: 2, background: 'white' }}/>
+              <Box width="100%" display="flex" flexDirection="column" p={2}>
+                <Typography variant="h3" component="p" fontWeight={500}>10+</Typography>
+                <Typography variant="subtitle1" component="p" fontWeight={400}>countries on our global delivery map</Typography>
+              </Box>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Container>
+    </section>
+  );
+}
--- src/app/_components/index.ts
+++ src/app/_components/index.ts
@@ -2,5 +2,6 @@ export { default as AppBar } from './AppBar';
 export { default as HeroSection } from './HeroSection';
 export { default as WhatWeDoSection } from './WhatWeDoSection';
 export { default as TechStackSection } from './TechStackSection';
+export { default as StatisticsWidget } from './StatisticsWidget';
 export { default as ContactUsSection } from './ContactUsSection';
 export { default as Footer } from './Footer';
