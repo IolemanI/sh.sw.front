88ef347e1e65ae10ce782fa6c6c148855734c6d4
Oleksii Carlini <oleksii.carlini@gmail.com>
2024-08-05 22:52:46 +0300

    updated app bar to temp. content

--- src/shared/components/AppBar/_components/DesktopAppBar/index.tsx
+++ src/shared/components/AppBar/_components/DesktopAppBar/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { AppBar, Box, Button, Container, Toolbar } from '@mui/material';
-import { pages } from '../../data';
+import { AppBar, Box, Button, Container, Link as MuiLink, Stack, Toolbar } from '@mui/material';
+// import { pages } from '../../data';
 
 export default function DesktopAppBar() {
   return (
@@ -19,17 +19,17 @@ export default function DesktopAppBar() {
             />
           </Link>
 
-          <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
-            {pages.map((page, i) => (
-              <Button
-                key={i}
-                onClick={() => null}
-                sx={{ my: 2, display: 'block' }}
-              >
-                {page.title}
-              </Button>
-            ))}
-          </Box>
+          <Stack sx={{ flexGrow: 1, justifyContent: 'center' }} direction="row" spacing={2}>
+            <MuiLink href="#who-we-are" underline="hover" variant="button">
+              Who We Are
+            </MuiLink>
+            <MuiLink href="#what-we-do" underline="hover" variant="button">
+              What We Do
+            </MuiLink>
+            <MuiLink href="#tech-stack" underline="hover" variant="button">
+              Tech Stack
+            </MuiLink>
+          </Stack>
 
           <Link href="/public#contact-us" style={{ flexGrow: 1, display: 'flex', justifyContent: 'end' }}>
             <Button
